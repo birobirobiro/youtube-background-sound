a83f957e8187837508a59512c208a5e82a1eea1e
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-10-16 19:52:35 -0300

    feat: full-screen

--- main.js
+++ main.js
@@ -119,4 +119,20 @@ function playPauseMedia(mediaId) {
   }
 }
 
+function fullScreen() {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
 initializeAudios();

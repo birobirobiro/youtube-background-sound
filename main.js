e00a647e4316713b95024630b79754f0ce758191
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-10-16 18:38:04 -0300

    remove comments and indented code

--- main.js
+++ main.js
@@ -2,61 +2,71 @@ tailwind.config = {
   daisyui: {
     themes: ["dracula"],
   },
-}
-
+};
 
 let player;
-let forestSound, fireplaceSound, oceanSound, keyboardSound, rainSound, cityRainSound;
+let forestSound,
+  fireplaceSound,
+  oceanSound,
+  keyboardSound,
+  rainSound,
+  cityRainSound;
 
-// Load the YouTube IFrame Player API code asynchronously.
-const tag = document.createElement('script');
-tag.src = 'https://www.youtube.com/iframe_api';
-const firstScriptTag = document.getElementsByTagName('script')[0];
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function initializeAudios() {
-  forestSound = document.getElementById('forestSound');
-  fireplaceSound = document.getElementById('fireplaceSound');
-  oceanSound = document.getElementById('oceanSound');
-  keyboardSound = document.getElementById('keyboardSound');
-  rainSound = document.getElementById('rainSound');
-  cityRainSound = document.getElementById('cityRainSound');
+  forestSound = document.getElementById("forestSound");
+  fireplaceSound = document.getElementById("fireplaceSound");
+  oceanSound = document.getElementById("oceanSound");
+  keyboardSound = document.getElementById("keyboardSound");
+  rainSound = document.getElementById("rainSound");
+  cityRainSound = document.getElementById("cityRainSound");
 }
 
 function setInitialVolumeForAudios(volume) {
-  [forestSound, fireplaceSound, oceanSound, keyboardSound, rainSound, cityRainSound].forEach(sound => {
+  [
+    forestSound,
+    fireplaceSound,
+    oceanSound,
+    keyboardSound,
+    rainSound,
+    cityRainSound,
+  ].forEach((sound) => {
     sound.volume = volume / 100;
   });
 }
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    videoId: 'ir96UnkKIgA', // Replace VIDEO_ID with the desired YouTube video ID
+  player = new YT.Player("player", {
+    videoId: "ir96UnkKIgA",
     playerVars: {
       controls: 0,
       showinfo: 0,
       modestbranding: 1,
       loop: 1,
-      playlist: 'ir96UnkKIgA', // Replace VIDEO_ID with the desired YouTube video ID
+      playlist: "ir96UnkKIgA",
       fs: 0,
       cc_load_policy: 0,
       iv_load_policy: 3,
       autohide: 0,
-      autoplay: 1 // Enable autoplay
+      autoplay: 1,
     },
     events: {
-      'onReady': onPlayerReady
-    }
+      onReady: onPlayerReady,
+    },
   });
 }
 
 function onPlayerReady(event) {
-  event.target.setVolume(5); // Set the YouTube player volume to 5 when the player is ready
-  setInitialVolumeForAudios(5); // Set the initial volume for the external audio elements
+  event.target.setVolume(5);
+  setInitialVolumeForAudios(5);
 }
 
 function loadVideo() {
-  const videoURL = document.getElementById('videoURL').value;
+  const videoURL = document.getElementById("videoURL").value;
   const videoID = extractVideoID(videoURL);
 
   if (player && videoID) {
@@ -65,19 +75,21 @@ function loadVideo() {
 }
 
 function changeVolume(target) {
-  if (target === 'player') {
+  if (target === "player") {
     if (player) {
-      const volume = document.getElementById('playerVolumeSlider').value;
+      const volume = document.getElementById("playerVolumeSlider").value;
       player.setVolume(volume);
     }
-  } else if (target === 'sound') {
-    const soundVolume = document.getElementById('soundVolumeSlider').value / 100;
+  } else if (target === "sound") {
+    const soundVolume =
+      document.getElementById("soundVolumeSlider").value / 100;
     setInitialVolumeForAudios(soundVolume * 100);
   }
 }
 
 function extractVideoID(url) {
-  const regExp = /(?:\?v=|\/embed\/|\/v\/|youtu\.be\/|\/watch\?v=|\/watch\?feature=player_embed&v=|\/videos\/|embed\/|watch\?v=|v=|\/embed\/)([a-zA-Z0-9_-]{11})/;
+  const regExp =
+    /(?:\?v=|\/embed\/|\/v\/|youtu\.be\/|\/watch\?v=|\/watch\?feature=player_embed&v=|\/videos\/|embed\/|watch\?v=|v=|\/embed\/)([a-zA-Z0-9_-]{11})/;
   const match = url.match(regExp);
   return (match && match[1]) || null;
 }
@@ -85,7 +97,7 @@ function extractVideoID(url) {
 let isVideoPlaying = false;
 
 function playPauseMedia(mediaId) {
-  if (mediaId === 'video') {
+  if (mediaId === "video") {
     if (player) {
       if (isVideoPlaying) {
         player.pauseVideo();

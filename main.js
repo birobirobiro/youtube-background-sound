b2221af639e2a5090b64bfaabab91cc160a2e7b0
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-10-17 09:19:12 -0300

    fix: full screen

--- main.js
+++ main.js
@@ -145,20 +145,48 @@ function playPauseMedia(mediaId) {
   }
 }
 
-function fullScreen() {
-  const elem = document.documentElement
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen()
-  } else if (elem.mozRequestFullScreen) {
-    /* Firefox */
-    elem.mozRequestFullScreen()
-  } else if (elem.webkitRequestFullscreen) {
-    /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen()
-  } else if (elem.msRequestFullscreen) {
-    /* IE/Edge */
-    elem.msRequestFullscreen()
+let isFullScreen = false;
+
+function toggleFullScreen() {
+  const elem = document.documentElement;
+  if (!isFullScreen) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+      document.msExitFullscreen();
+    }
   }
 }
+
+// Event listener for the full screen toggle
+document.addEventListener("fullscreenchange", () => {
+  isFullScreen = !isFullScreen;
+});
+
+document.addEventListener("mozfullscreenchange", () => {
+  isFullScreen = !isFullScreen;
+});
+
+document.addEventListener("webkitfullscreenchange", () => {
+  isFullScreen = !isFullScreen;
+});
+
+document.addEventListener("msfullscreenchange", () => {
+  isFullScreen = !isFullScreen;
+});
 
 initializeAudios()

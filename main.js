c1396c3575d327cda3d01917461ccc61f1353487
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-10-25 16:02:36 -0300

    Revert "style(border): invisible border to remove size change effect"

--- main.js
+++ main.js
@@ -134,7 +134,7 @@ function extractVideoID(url) {
 
 let isVideoPlaying = false
 
-function playPauseMedia(event,mediaId) {
+function playPauseMedia(mediaId) {
   if (mediaId === "video") {
     if (player) {
       if (isVideoPlaying) {
@@ -147,20 +147,11 @@ function playPauseMedia(event,mediaId) {
     }
   } else {
     const sound = document.getElementById(mediaId)
-    const element = event.target
-    const button = element.parentNode
     if (sound) {
       if (sound.paused) {
         sound.play()
-        if (button) {
-          button.classList.add("border-inherit")
-        }
       } else {
         sound.pause()
-        if (button) {
-          button.classList.remove('border-inherit')
-        }
-
       }
     }
   }
